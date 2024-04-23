bc74173dde79c2f66d3f800227b481d552738c34
Maksim Andruhin <maks.andruhin@gmail.com>
2024-04-24 01:26:43 +0300

    solve problem 169 (majority element)

---
+++ Problems/MajorityElement_169.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    internal class MajorityElement_169
+    {
+        public int MajorityElement(int[] nums)
+        {
+            var entryCountByElement = new Dictionary<int, int>();
+
+            foreach (int num in nums)
+            {
+                if (!entryCountByElement.ContainsKey(num))
+                {
+                    entryCountByElement.Add(num, 0);
+                }
+
+                entryCountByElement[num]++;
+            }
+
+            return entryCountByElement.MaxBy(keyValuePair => keyValuePair.Value).Key;
+        }
+
+        public void Test()
+        {
+            int[] num = [2, 2, 1, 1, 1, 2, 2];
+
+            var k = MajorityElement(num);
+
+            Console.WriteLine();
+        }
+    }
+}
