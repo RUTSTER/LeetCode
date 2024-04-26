661620a86ee716b348f46e634544628b938b3e38
Maksim Andruhin <maks.andruhin@gmail.com>
2024-04-26 05:09:28 +0300

    save problem 55 (jump game)

---
+++ Problems/JumpGame_55.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    internal class JumpGame_55
+    {
+        public bool CanJump(int[] nums)
+        {
+            var lastIndex = nums.Length - 1;
+
+            if (lastIndex <= nums[0])
+            {
+                return true;
+            }
+
+            var max = nums[0];
+            var current = 0;
+
+            while (current < max)
+            {
+                current++;
+                if (max < current + nums[current])
+                {
+                    max = current + nums[current];
+
+                    if (max >= lastIndex)
+                    {
+                        return true;
+                    }                   
+                }                
+            }
+
+            return false;
+        }
+
+        public void Test()
+        {
+            int[] num = [2, 0, 6, 9, 8, 4, 5, 0, 8, 9, 1, 2, 9, 6, 8, 8, 0, 6, 3, 1, 2, 2, 1, 2,
+                6, 5, 3, 1, 2, 2, 6, 4, 2, 4, 3, 0, 0, 0, 3, 8, 2, 4, 0, 1, 2, 0, 1, 4, 6, 5, 8,
+                0, 7, 9, 3, 4, 6, 6, 5, 8, 9, 3, 4, 3, 7, 0, 4, 9, 0, 9, 8, 4, 3, 0, 7, 7, 1, 9,
+                1, 9, 4, 9, 0, 1, 9, 5, 7, 7, 1, 5, 8, 2, 8, 2, 6, 8, 2, 2, 7, 5, 1, 7, 9, 6, 1];
+
+            var k = CanJump(num);
+
+            Console.WriteLine();
+        }
+    }
+}
