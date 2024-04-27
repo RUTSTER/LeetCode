a26de11e2b596f0d6ae6d0b3755d12a30c6ac5f1
Maksim Andruhin <maks.andruhin@gmail.com>
2024-04-27 14:50:57 +0300

    solve problem 274 (h-index)

---
+++ Problems/HIndex_274.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    internal class HIndex_274
+    {
+        public int HIndex(int[] citations)
+        {
+            Array.Sort(citations, (a, b) => b.CompareTo(a));
+            for (int i = 0; i < citations.Length; i++)
+            {
+                if (i >= citations[i])
+                {
+                    return i;
+                }
+            }
+            return citations.Length;
+        }
+
+        public void Test()
+        {
+            int[] nums = [1, 3, 1];
+
+            var k = HIndex(nums);
+
+            Console.WriteLine();
+        }
+    }
+}
