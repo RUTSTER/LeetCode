8cfff4ef3b197696c0467118494abfc475fc3af9
Maksim Andruhin <maks.andruhin@gmail.com>
2024-04-28 02:06:09 +0300

    solve problem 238 (product of array except self)

---
+++ Problems/ProductOfArrayExceptSelf_238.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    internal class ProductOfArrayExceptSelf_238
+    {
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        var answer = new int[nums.Length];
+
+        var moveRight = 1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            answer[i] = moveRight;
+            moveRight *= nums[i];
+        }
+
+        var moveLeft = 1;
+        for (int i = nums.Length - 1; i >= 0; i--)
+        {
+            answer[i] *= moveLeft;
+            moveLeft *= nums[i];
+        }
+
+        return answer;
+    }
+
+        public void Test() 
+        {
+            int[] nums = [1, 2, 3, 4];
+
+            var res = ProductExceptSelf(nums);
+
+            Console.WriteLine();
+        }
+    }
+}
