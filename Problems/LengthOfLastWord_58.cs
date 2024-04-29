a0252e056eff0e8f60060cfd939b5d506fcbc50f
Maksim Andruhin <maks.andruhin@gmail.com>
2024-04-29 23:33:07 +0300

    solve problem 58 (length of last word)

---
+++ Problems/LengthOfLastWord_58.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    internal class LengthOfLastWord_58
+    {
+        public int LengthOfLastWord(string s)
+        {
+            var lastChar = s.Length - 1;
+            var length = 0;
+            for (int i = lastChar; i >= 0; i--)
+            {
+                if (i == lastChar && s[lastChar] == ' ')
+                {
+                    while (s[i] == ' ')
+                    {
+                        i--;
+                    }
+                }
+
+                if (s[i] == ' ')
+                {
+                    return length;
+                }
+
+                length++;
+            }
+
+            return 0;
+        }
+
+        public void Test()
+        {
+            var str = "   fly me   to   the moon  ";
+
+            var k = LengthOfLastWord(str);
+
+            Console.WriteLine();
+        }
+    }
+}
