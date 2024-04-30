b55d7a06d1c2c56196fe6743c62df40f79c5077b
Maksim Andruhin <maks.andruhin@gmail.com>
2024-04-30 19:12:01 +0300

    solve problem 14 (longest commonn prefix)

---
+++ Problems/LongestCommonPrefix_14.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    internal class LongestCommonPrefix_14
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            var prefix = new StringBuilder();
+
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                var currentChar = strs[0][i];
+                foreach (var str in strs)
+                {
+                    if (str.Length <= i || currentChar != str[i])
+                    {
+                        return prefix.ToString();
+                    }
+                }
+
+                prefix.Append(currentChar);
+            }
+
+            return prefix.ToString();
+        }
+
+        public void Test()
+        {
+            string[] strs = ["flower", "flow", "flight"];
+
+            var k = LongestCommonPrefix(strs);
+
+            Console.WriteLine();
+        }
+    }
+}
