820bb0fb2100a6441a8c07f6ec316554a26e8616
Maksim Andruhin <maks.andruhin@gmail.com>
2024-05-02 23:14:02 +0300

    solve problem 151 (reverse words in string)

---
+++ Problems/ReverseWordsInString_151.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    internal class ReverseWordsInString_151
+    {
+        public string ReverseWords(string s)
+        {
+            var reversed = s.Split(' ', StringSplitOptions.RemoveEmptyEntries).Reverse();
+            return string.Join(' ', reversed);
+        }
+
+        public void Test()
+        {
+            var triangle = "  the  sky is blue";
+
+            var k = ReverseWords(triangle);
+
+            Console.WriteLine();
+        }
+    }
+}
