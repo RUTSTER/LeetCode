4d7c63c9b0561c47e243cc21f105526858889bcc
Maksim Andruhin <maks.andruhin@gmail.com>
2024-05-03 20:31:51 +0300

    solve problem 392 (is subsequence)

---
+++ Problems/IsSubSequence_392.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    internal class IsSubSequence_392
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            if (s.Equals(""))
+            {
+                return true;
+            }
+
+            if (t.Equals(""))
+            {
+                return false;
+            }
+
+            var subIndex = 0;
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (t[i] != s[subIndex])
+                {
+                    continue;
+                }
+
+                subIndex++;
+
+                if (subIndex == s.Length)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Test()
+        {
+            var subsequence = "abcw";
+            var str = "ahbgdc";
+
+            var k = IsSubsequence(subsequence, str);
+
+            Console.WriteLine();
+        }
+    }
+}
