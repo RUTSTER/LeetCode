9644ad438ce3a9ef402e3db13ff615e11d170c7e
Maksim Andruhin <maks.andruhin@gmail.com>
2024-05-04 22:03:42 +0300

    solve problem 125 (valid palindrome)

---
+++ CsProblems/Problems/ValidPalindrome_125.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsProblems.Problems
+{
+    internal class ValidPalindrome_125
+    {
+    public bool IsPalindrome(string s)
+    {
+        var lastI = s.Length - 1;
+
+        if (lastI == 0)
+        {
+            return true;
+        }
+
+        var forwardI = 0;
+        var backwardI = 0;
+        while (forwardI + backwardI <= lastI)
+        {
+            if (!char.IsLetterOrDigit(s[forwardI]))
+            {
+                forwardI++;
+                continue;
+            }
+
+            if (!char.IsLetterOrDigit(s[lastI - backwardI]))
+            {
+                backwardI++;
+                continue;
+            }
+
+            if (char.ToLower(s[forwardI]) != char.ToLower(s[lastI - backwardI]))
+            {
+                return false;
+            }
+
+            forwardI++;
+            backwardI++;
+        }
+
+        return true;
+    }
+
+
+        public void Test()
+        {
+            var s = "A man, a plan, a canal: Panama";
+
+            var k = IsPalindrome(s);
+
+            Console.WriteLine();
+        }
+    }
+}
