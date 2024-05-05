0638bc3abad395af0b29cdd0fbf83f72f70739a4
Maksim Andruhin <maks.andruhin@gmail.com>
2024-05-05 17:32:19 +0300

    solve problem 383 (ransom note)

---
+++ CsProblems/Problems/RansomNote_383.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsProblems.Problems
+{
+    internal class RansomNote_383
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            var ransomNoteCharsEntries = GetCharsEntries(ransomNote);
+            var magazineCharsEntries = GetCharsEntries(magazine);
+
+            foreach (var ransomNoteChar in ransomNoteCharsEntries.Keys)
+            {
+                if (!magazineCharsEntries.ContainsKey(ransomNoteChar))
+                {
+                    return false;
+                }
+
+                if (magazineCharsEntries[ransomNoteChar] < ransomNoteCharsEntries[ransomNoteChar])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private Dictionary<char, int> GetCharsEntries(string str)
+        {
+            var charsCount = new Dictionary<char, int>();
+
+            foreach (var ch in str)
+            {
+                if (!charsCount.ContainsKey(ch))
+                {
+                    charsCount[ch] = 0;
+                }
+
+                charsCount[ch]++;
+            }
+
+            return charsCount;
+        }
+
+        public void Test()
+        {
+            var ransomNote = "aa";
+            var magazine = "aab";
+
+            var k = CanConstruct(ransomNote, magazine);
+
+            Console.WriteLine();
+        }
+    }
+}
