ebf35ac63e9a7e057b5fa1973c433cd3e41e202a
Maksim Andruhin <maks.andruhin@gmail.com>
2024-05-07 22:53:29 +0300

    [cs] solve problem 1 (two sum)

---
+++ CsProblems/Problems/TwoSum_1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    internal class TwoSum_1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var requiredValueTillTargetToIndex = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (requiredValueTillTargetToIndex.ContainsKey(nums[i]))
+                {
+                    return [requiredValueTillTargetToIndex[nums[i]], i];
+                }
+
+                requiredValueTillTargetToIndex[target - nums[i]] = i;
+            }
+
+            return [0, 1];
+        }
+
+
+        public void Test()
+        {
+            int[] nums = [2, 7, 11, 15];
+            var target = 17;
+
+            var k = TwoSum(nums, target);
+
+            Console.WriteLine();
+        }
+    }
+}
