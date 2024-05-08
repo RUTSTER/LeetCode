9fe0801cd11896f1fc4a7ea57955062d665c38b4
Maksim Andruhin <maks.andruhin@gmail.com>
2024-05-08 22:11:26 +0300

    [cs] solve problem 205 (isomorphic strings)

---
+++ CsProblems/Problems/IsomorphicStrings_205.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsProblems.Problems
+{
+    internal class IsomorphicStrings_205
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            var mapSToT = new Dictionary<char, char>();
+            var mappedT = new HashSet<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (mapSToT.ContainsKey(s[i]) ^ mappedT.Contains(t[i]))
+                {
+                    return false;
+                }
+
+                if (!mapSToT.ContainsKey(s[i]))
+                {
+                    mapSToT[s[i]] = t[i];
+                    mappedT.Add(t[i]);
+                    continue;
+                }
+
+                if (mapSToT[s[i]] != t[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+        public void Test()
+        {
+            var s = "aba";
+            var t = "122";
+
+            var k = IsIsomorphic(s, t);
+
+            Console.WriteLine();
+        }
+    }
+}
