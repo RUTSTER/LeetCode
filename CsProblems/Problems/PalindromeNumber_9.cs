a0ecd46cd9be85a52470c1f70b334907c3362328
Maksim Andruhin <maks.andruhin@gmail.com>
2024-08-04 07:41:07 +0300

    [cs] solve problem 9 (palindrome number)

---
+++ CsProblems/Problems/PalindromeNumber_9.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsProblems.Problems
+{
+    internal class PalindromeNumber_9
+    {
+        public bool IsPalindrome(int x)
+        {            
+            var strX = x.ToString();
+            if (strX.Length < 2)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < strX.Length / 2; i++)
+            {
+                if (strX[i] != strX[^(i+1)])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+        public void Test()
+        {
+            var x = 1392931;
+
+
+            var res = IsPalindrome(x);
+
+            Console.WriteLine();
+        }
+    }
+}
