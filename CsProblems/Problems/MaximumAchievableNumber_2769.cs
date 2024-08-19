c5c935656b6e1af150923ad008fba227e9be5af5
Maksim Andruhin <maks.andruhin@gmail.com>
2024-08-19 05:20:28 +0300

    [cs] solve problem 2769 (maximum achievable number)

---
+++ CsProblems/Problems/MaximumAchievableNumber_2769.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsProblems.Problems
+{
+    internal class MaximumAchievableNumber_2769
+    {
+        public int TheMaximumAchievableX(int num, int t)
+        {
+            return num + (t * 2);
+        }
+
+
+        public void Test()
+        {
+            var num = 3;
+            var t = 2;
+
+            var res = TheMaximumAchievableX(num, t);
+
+            Console.WriteLine();
+        }
+    }
+}
