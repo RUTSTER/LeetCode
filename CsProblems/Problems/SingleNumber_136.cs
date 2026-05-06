270b9ce8030409c2ec20aa1bd58a8c81af56e767
Maksim Andruhin <maks.andruhin@gmail.com>
2026-05-06 13:13:08 +0300

    [cs] solve problem 136 (single number)

---
+++ CsProblems/Problems/SingleNumber_136.cs
@@ -0,0 +1,24 @@
+﻿namespace CsProblems.Problems;
+
+internal class SingleNumber_136
+{
+    public int SingleNumber(int[] nums)
+    {
+        var buf = 0;
+        
+        foreach (var n in nums)
+        {
+            buf ^= n;
+        }
+
+        return buf;
+    }
+
+
+    public void Test()
+    {
+        var res = SingleNumber([4,1,2,1,2]);
+
+        Console.WriteLine(res);
+    }
+}
